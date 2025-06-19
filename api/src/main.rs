15b74672fb6734a85b39043308b196b9459b314b
jeevan4476 <jeevanr2431@gmail.com>
2025-06-20 00:21:04 +0530

    added serde:

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,15 +1,33 @@
 use poem::{
-    EndpointExt, Route, Server, get, handler, listener::TcpListener, middleware::Tracing, web::Path,
+    EndpointExt, Route, Server, get, handler,
+    listener::TcpListener,
+    middleware::Tracing,
+    post,
+    web::{Json, Path},
 };
 
+use crate::{req_inputs::CreateWebsiteInput, req_outpus::CreateWebsiteOutput};
+
+pub mod req_inputs;
+pub mod req_outpus;
+
 #[handler]
-fn hello(Path(name): Path<String>) -> String {
+fn get_website(Path(name): Path<String>) -> String {
     format!("Hello, {}!", name)
+}
+
+#[handler]
+fn create_websites(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOutput> {
+    let reponse = CreateWebsiteOutput { id: data.url };
+
+    Json(reponse)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let app = Route::new().at("/hello/:name", get(hello)).with(Tracing);
+    let app = Route::new()
+        .at("/website/:website_id", get(get_website))
+        .at("/website", post(create_websites));
     Server::new(TcpListener::bind("0.0.0.0:3000"))
         .name("hellow world")
         .run(app)
